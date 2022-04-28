4cb3d058a7195103becb5a30b741f239a6394d5d
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2022-04-28 12:41:16 -0300

    fix:Parking Control Delete service

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -42,7 +42,7 @@ public class ParkingSpotService {
     public Optional<ParkingSpotModel> findById(UUID id) {
         return parkingSpotRepository.findById(id);
     }
-
+    @Transactional
     public void delete(ParkingSpotModel parkingSpotModel) {
         parkingSpotRepository.delete(parkingSpotModel);
     }
